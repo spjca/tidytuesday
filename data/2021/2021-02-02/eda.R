f830465a17961ab9f14d0071ad8bccad5e773784
rsm-spjackso <s.jackson.ca@gmail.com>
2021-02-05 18:35:15 -0800

    eda
    initial gifs

--- data/2021/2021-02-02/eda.R
+++ data/2021/2021-02-02/eda.R
@@ -19,17 +19,17 @@ hbcu_perc <- hbcu_all %>%
 ggplot(hbcu_perc, aes(x = Year,y= Total,fill = Gender)) +
   geom_area()
 
-hbcu_perc <- hbcu_perc %>%
+hbcu_perc_area <- hbcu_perc %>%
   group_by(Year, Gender) %>%
   summarise(n = sum(Total)) %>%
   mutate(Percentage = n / sum(n))
 
-ggplot(hbcu_perc, aes(x = Year, y = Percentage, fill = Gender)) +
+ggplot(hbcu_perc_area, aes(x = Year, y = Percentage, fill = Gender)) +
   geom_area()
 
 # now fancy it up
-ggplot(hbcu_perc, aes(x = Year, y = Percentage, fill = Gender)) +
-  geom_area(alpha=0.6 , size=.5, colour="white") +
+ggplot(hbcu_perc_area, aes(x = Year, y = Percentage, fill = Gender)) +
+  geom_area(alpha=0.6 , size=.5, colour="black") +
   scale_fill_viridis(discrete = T) +
   theme_ipsum() +
   theme(legend.position="bottom") +
@@ -43,3 +43,53 @@ ggplot(hbcu_perc, aes(x = Year, y = Percentage, fill = Gender)) +
 # https://thatdatatho.com/2020/03/28/tidyrs-pivot_longer-and-pivot_wider-examples-tidytuesday-challenge/
 # http://www.sthda.com/english/wiki/ggplot2-legend-easy-steps-to-change-the-position-and-the-appearance-of-a-graph-legend-in-r-software#change-the-legend-position
 # 
+
+
+hbcu_colleges_perc <- hbcu_all %>%
+  select(Year,`2-year - Public`,`2-year - Private`,`4-year - Public`,`4-year - Private`) %>%
+  pivot_longer(cols = c(`2-year - Public`,`2-year - Private`,`4-year - Public`,`4-year - Private`), 
+               names_to = "College Type", 
+               values_to = "Total")
+
+ggplot(hbcu_colleges_perc, aes(x = Year,y= Total,fill = `College Type`)) +
+  geom_area()
+
+hbcu_colleges_perc_area <- hbcu_colleges_perc %>%
+  group_by(Year,`College Type`) %>%
+  summarise(n = sum(Total)) %>%
+  mutate(Percentage = n / sum(n))
+
+
+# now fancy it up
+p <- ggplot(hbcu_colleges_perc_area, aes(x = Year, y = Percentage, fill = `College Type`)) +
+  geom_area(alpha=0.6 , size=.5, colour="black") +
+  scale_fill_viridis(discrete = T) +
+  theme_ipsum() +
+  theme(legend.position="bottom") +
+  labs(x="Year", y="Percent",
+       subtitle="1976 to 2015",
+       caption="SeanPJ.com") +
+  ggtitle("HBCU by Type") 
+
+# and make it animated
+library(gganimate)
+
+q <- p + transition_reveal(Year)
+
+animate(q, end_pause = 10)
+anim_save('area.gif', q)
+
+
+t <- ggplot(hbcu_colleges_perc, aes(x = Year, y = Total, fill = `College Type`)) +
+  geom_area(alpha=0.6 , size=.5, colour="black") +
+  scale_fill_viridis(discrete = T) +
+  scale_y_continuous(breaks = c(0,50000,100000,150000,200000,250000,300000,350000) )+
+  theme_ipsum() +
+  theme(legend.position="bottom") +
+  labs(x="Year", y="Total",
+       subtitle="1976 to 2015",
+       caption="SeanPJ.com") +
+  ggtitle("HBCU by Type") +
+  transition_reveal(Year)
+
+anim_save('output.gif',t)
