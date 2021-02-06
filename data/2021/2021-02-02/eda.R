6468090a553a3d3bd12cae8fe6b65a50b451be50
rsm-spjackso <s.jackson.ca@gmail.com>
2021-02-05 20:32:39 -0800

    finished
    added a facet_wrap animation and rmd pushed to rpubs

--- data/2021/2021-02-02/eda.R
+++ data/2021/2021-02-02/eda.R
@@ -44,10 +44,16 @@ ggplot(hbcu_perc_area, aes(x = Year, y = Percentage, fill = Gender)) +
 # http://www.sthda.com/english/wiki/ggplot2-legend-easy-steps-to-change-the-position-and-the-appearance-of-a-graph-legend-in-r-software#change-the-legend-position
 # 
 
+#make the names easier to work with
+hbcu_all <- hbcu_all %>%
+  rename('Public2Year' = `2-year - Public` ,
+         'Private2Year' = `2-year - Private`,
+         'Public4Year' = `4-year - Public`,
+         'Private4Year' = `4-year - Private`)
 
 hbcu_colleges_perc <- hbcu_all %>%
-  select(Year,`2-year - Public`,`2-year - Private`,`4-year - Public`,`4-year - Private`) %>%
-  pivot_longer(cols = c(`2-year - Public`,`2-year - Private`,`4-year - Public`,`4-year - Private`), 
+  select(Year,Public2Year,Private2Year,Public4Year,Private4Year) %>%
+  pivot_longer(cols = c(Public2Year,Private2Year,Public4Year,Private4Year), 
                names_to = "College Type", 
                values_to = "Total")
 
@@ -67,16 +73,16 @@ p <- ggplot(hbcu_colleges_perc_area, aes(x = Year, y = Percentage, fill = `Colle
   theme_ipsum() +
   theme(legend.position="bottom") +
   labs(x="Year", y="Percent",
-       subtitle="1976 to 2015",
+       subtitle="Annual Attendance By University Type 1976 to 2015",
        caption="SeanPJ.com") +
-  ggtitle("HBCU by Type") 
-
+  ggtitle("Historically Black Colleges and Universities")
+  
 # and make it animated
 library(gganimate)
 
 q <- p + transition_reveal(Year)
 
-animate(q, end_pause = 10)
+animate(p, end_pause = 10)
 anim_save('area.gif', q)
 
 
@@ -87,9 +93,9 @@ t <- ggplot(hbcu_colleges_perc, aes(x = Year, y = Total, fill = `College Type`))
   theme_ipsum() +
   theme(legend.position="bottom") +
   labs(x="Year", y="Total",
-       subtitle="1976 to 2015",
+       subtitle="Annual Attendance By University Type 1976 to 2015",
        caption="SeanPJ.com") +
-  ggtitle("HBCU by Type") +
+  ggtitle("Historically Black Colleges and Universities") +
   transition_reveal(Year)
 
 anim_save('output.gif',t)
